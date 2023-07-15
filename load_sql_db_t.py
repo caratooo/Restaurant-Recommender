e0df262c578ab98a8b16d33030f7c85f80e923a5
caratooo <107289876+caratooo@users.noreply.github.com>
2023-07-14 23:20:09 -0400

    updated reader and sql table

---
+++ load_sql_db_t.py
@@ -0,0 +1,48 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="testing.c5lscidqsauo.us-east-1.rds.amazonaws.com",
+    user="zoey",
+    passwd="533Qu13L!y4Y!",
+    database="testdatabase"
+)
+
+mycursor = db.cursor()
+
+# mycursor.execute("CREATE DATABASE testdatabase")
+
+# name = row[0]
+# rating = row[1]
+# num_reviews = row[2]
+# resto_type = row[3]
+# price_point = row[4]
+# address = row[5]
+# about = row[6]
+# personal = row[7]
+
+Q1 = """CREATE TABLE Restos (
+    resto_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    resto_name VARCHAR(50) NOT NULL, 
+    rating FLOAT(2) NOT NULL, 
+    num_reviews INT(5) NOT NULL, 
+    resto_type VARCHAR(50) NOT NULL, 
+    price_point VARCHAR(5),
+    address VARCHAR(50) NOT NULL,
+    personal_note VARCHAR(200) NOT NULL
+    )  
+    """
+
+# mycursor.execute(Q1)
+# db.commit()
+
+# mycursor.execute("DROP TABLE Restos")
+# db.commit()
+
+mycursor.execute("DESCRIBE Restos")
+for x in mycursor:
+    print(x) 
+
+
+
+
+
--- reader.py
+++ reader.py
@@ -10,12 +10,31 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-def read_restos(file: str) -> Graph:
+def read_restos(file: str):
     """
     """
     with open(file, ncoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
         header = next(reader)
 
+        Q1 = """INSERT INTO Restos (resto_ID, resto_name, rating, num_reviews, resto_type, price_point, address, about, personal_note)
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """
+        
         for row in reader:
-            pass
+            name = row[0]
+            rating = row[1]
+            num_reviews = row[2]
+            resto_type = row[3]
+            price_point = row[4]
+            address = row[5]
+            personal = row[7]
+            
+            if price_point == '':
+                mycursor.execute("""INSERT INTO Restos (resto_ID, resto_name, rating, num_reviews, resto_type, price_point, address, about, personal_note) 
+                                 VALUES(name, rating, num_reviews, resto_type, NULL, address, personal)
+                                 """)
+            else:
+                mycursor.execute(Q1, (name, rating, num_reviews, resto_type, price_point, address, personal))
+
+
